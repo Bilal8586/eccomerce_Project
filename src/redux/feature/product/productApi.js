514e70b8fdb44b25fc42b25c86baff36bb9b743f
Bilal8586 <bilaluddin3783@gmail.com>
2023-09-02 13:20:18 +0530

    update

--- src/redux/feature/product/productApi.js
+++ src/redux/feature/product/productApi.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 export const fetchProdut = ()=>{
-    const data =axios.get('http://localhost:3000/api/products')
-  // const data =axios.get('https://eccomerce-data.onrender.com/products')
-
-    // console.log('data',data)
-    
+    const data =axios.get('https://eccomerce-project-six.vercel.app/api/products')
+  
     return data
 
 }
